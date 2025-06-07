b30982a60f79e324ef3512cb864434c76390e722
estlinEE <166226291+estlinEE@users.noreply.github.com>
2025-06-07 09:39:33 +0300

    Update driveService.js
    git add driveService.js scheduleManager.js index.html
    git commit -m "Enable CORS proxy for Drive API"
    git push

--- driveService.js
+++ driveService.js
@@ -1,4 +1,4 @@
-// driveService.js
+// driveService.jsa
 const WEB_APP_URL = 'https://script.google.com/a/macros/saksa.tln.edu.ee/s/AKfycbxZtgo8EZ9j7qtt6YvdnQt2FrgMNDnXvlfsxN-EYthXwa9IjYpxCMMYxJe95z4tTdgBqg/exec';  // sinu Apps Script URL
 const API_URL     = 'https://cors-anywhere.herokuapp.com/' + WEB_APP_URL;
 
